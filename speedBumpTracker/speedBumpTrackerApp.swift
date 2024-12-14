08ac973639b966ff52f31529266aa0af3825b553
Gerrie Diaz <pronouncedJerry@users.noreply.github.com>
2024-12-14 13:05:02 -0500

    Initial Commit

---
+++ speedBumpTracker/speedBumpTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  speedBumpTrackerApp.swift
+//  speedBumpTracker
+//
+//  Created by Gerrie Diaz on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct speedBumpTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ speedBumpTrackerTests/speedBumpTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  speedBumpTrackerTests.swift
+//  speedBumpTrackerTests
+//
+//  Created by Gerrie Diaz on 12/14/24.
+//
+
+import Testing
+@testable import speedBumpTracker
+
+struct speedBumpTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
